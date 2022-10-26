b33ed596babc16e835c3f2600dd1dc3996b119d1
Mathis_dvg <Tysma.desvigne@gmail.com>
2022-10-26 22:45:39 +0200

    Changement: Html, css et js en entier (switch d'image réussi )

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function changeImage(filename) {
-                    let img = document.querySelector("#clickstart")
-                    img.setAttribute("src", "filename");
-                }
-
+   function changeImage(filename) {
+            let img = document.getElementById('clickstart');
+                img.setAttribute("src", filename);
+                img.style.height = '1080px';
+                img.style.width = '1920px';
+                img.style.top = '0%'
+                img.style.left = '0%'}
