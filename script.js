ae52def8755ae82e621b794d96e265cda0dfea4a
Mathis_dvg <Tysma.desvigne@gmail.com>
2022-10-27 14:24:00 +0200

    rajout d'image

--- script.js
+++ script.js
@@ -5,3 +5,10 @@
                 img.style.width = '100%';
                 img.style.top = '0%'
                 img.style.left = '0%'}
+
+
+   function remplace_image_par_vdo() 
+{
+   var mon_id = document.getElementById('zone_contenant_image');
+   mon_id.innerHTML = "<video controls autoplay><source src=\"file:///C:/Users/mdesv/OneDrive/Bureau/Dosier%20NSI/Projet_NSI_POINCA/Images/1_Reveille/1_Reveille_Video1.mp4\" type=\"video/mp4\"></video>";
+}
